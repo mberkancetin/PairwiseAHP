ed46714c84b1d651f93c9257d48e287a7cb69621
Mahmut Berkan Cetin <155624332+mberkancetin@users.noreply.github.com>
2025-11-10 22:46:36 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // IMPORTANT: Replace 'your-repo-name' with the actual name of your GitHub repository.
-  base: '/your-repo-name/', 
+  base: '/PairwiseAHP/', 
   plugins: [react()],
 })
