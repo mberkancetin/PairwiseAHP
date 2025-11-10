88458236ec5e3296083d5937631a2c6ff7a0baa7
Mahmut Berkan Cetin <155624332+mberkancetin@users.noreply.github.com>
2025-11-10 19:26:49 +0100

    Create data.ts

---
+++ data.ts
@@ -0,0 +1,42 @@
+export const surveyData = [
+  // Main Dimension 1: Operational Integrity
+  {
+    key: 'main_criterion_1',
+    subCriteria: {
+      'mc1_sc1': ['mc1_sc1_ex1', 'mc1_sc1_ex2', 'mc1_sc1_ex3', 'mc1_sc1_ex4'],
+      'mc1_sc2': ['mc1_sc2_ex1', 'mc1_sc2_ex2', 'mc1_sc2_ex3', 'mc1_sc2_ex4'],
+      'mc1_sc3': ['mc1_sc3_ex1', 'mc1_sc3_ex2', 'mc1_sc3_ex3', 'mc1_sc3_ex4'],
+      'mc1_sc4': ['mc1_sc4_ex1', 'mc1_sc4_ex2', 'mc1_sc4_ex3', 'mc1_sc4_ex4'],
+    }
+  },
+  // Main Dimension 2: Societal Alignment
+  {
+    key: 'main_criterion_2',
+    subCriteria: {
+      'mc2_sc1': ['mc2_sc1_ex1', 'mc2_sc1_ex2', 'mc2_sc1_ex3', 'mc2_sc1_ex4'],
+      'mc2_sc2': ['mc2_sc2_ex1', 'mc2_sc2_ex2', 'mc2_sc2_ex3', 'mc2_sc2_ex4'],
+      'mc2_sc3': ['mc2_sc3_ex1', 'mc2_sc3_ex2', 'mc2_sc3_ex3', 'mc2_sc3_ex4'],
+      'mc2_sc4': ['mc2_sc4_ex1', 'mc2_sc4_ex2', 'mc2_sc4_ex3', 'mc2_sc4_ex4'],
+    }
+  },
+  // Main Dimension 3: Regulatory Compliance
+  {
+    key: 'main_criterion_3',
+    subCriteria: {
+      'mc3_sc1': ['mc3_sc1_ex1', 'mc3_sc1_ex2', 'mc3_sc1_ex3', 'mc3_sc1_ex4'],
+      'mc3_sc2': ['mc3_sc2_ex1', 'mc3_sc2_ex2', 'mc3_sc2_ex3', 'mc3_sc2_ex4'],
+      'mc3_sc3': ['mc3_sc3_ex1', 'mc3_sc3_ex2', 'mc3_sc3_ex3', 'mc3_sc3_ex4'],
+      'mc3_sc4': ['mc3_sc4_ex1', 'mc3_sc4_ex2', 'mc3_sc4_ex3', 'mc3_sc4_ex4'],
+    }
+  },
+  // Main Dimension 4: Stakeholder Collaboration
+  {
+    key: 'main_criterion_4',
+    subCriteria: {
+      'mc4_sc1': ['mc4_sc1_ex1', 'mc4_sc1_ex2', 'mc4_sc1_ex3', 'mc4_sc1_ex4'],
+      'mc4_sc2': ['mc4_sc2_ex1', 'mc4_sc2_ex2', 'mc4_sc2_ex3', 'mc4_sc2_ex4'],
+      'mc4_sc3': ['mc4_sc3_ex1', 'mc4_sc3_ex2', 'mc4_sc3_ex3', 'mc4_sc3_ex4'],
+      'mc4_sc4': ['mc4_sc4_ex1', 'mc4_sc4_ex2', 'mc4_sc4_ex3', 'mc4_sc4_ex4'],
+    }
+  }
+];
